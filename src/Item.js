e8307bc82106033a9445ffbabcb95ac19866c503
nikhil <nikhiln2959@gmail.com>
2022-05-19 21:23:36 -0700

    second commit

--- src/Item.js
+++ src/Item.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { bool, string } from "prop-types";
 import Magnifier from "react-magnifier";
+//import { Swiper, SwiperSlide } from 'swiper/react';
 
 
 const Item = React.memo(
@@ -11,8 +12,23 @@ const Item = React.memo(
   }) => {
       const itemSrc = isFullscreen ? fullscreen || original : original;
       return (
+{//Tried implementing swipe function along with magnifier but I'm unable to achieve both of them simultaneously,
+  //Either I am able to execute swipe functionality only or magnifier only
+  
+  /* <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper> */}
           <Magnifier
-              
+            
               src={itemSrc}
               width="100%"
               height="80%"
